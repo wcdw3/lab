5ff99b5c43259672e95781f4815be2a5e70eeea0
gihyeon.wes.lee <wes5510@freshmentor.com>
2022-06-24 17:12:50 +0900

    feat(chakra-ui): add packages

---
+++ packages/chakraUi/src/App.tsx
@@ -0,0 +1,11 @@
+import { Heading, Stack } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Stack p="8" spacing={6}>
+      <Heading>Chakra UI Lab</Heading>
+    </Stack>
+  );
+}
+
+export default App
